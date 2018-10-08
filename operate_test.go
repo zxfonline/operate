af54daaba2c4a7f940ea19a6f64c40b4c7f653f4
zhangxianfu <359168950@qq.com>
2018-10-08 17:19:19 +0800

    Update operate_test.go

--- operate_test.go
+++ operate_test.go
@@ -5,18 +5,18 @@ import "testing"
 func TestOperate(t *testing.T) {
 	op, err := NewOperate("(-2.2+a-(l/20)I)I")
 	if err != nil {
-		t.Fatalf("NewOperate failed. err:%v", err)
+		t.Fatalf("NewOperate failed. err:%v.", err)
 	}
 	value, err := op.Execute([]string{"l", "60", "a", "-10"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != -15 {
 		t.Errorf("Execute failed. Got %v, expected -15.", value)
 	}
 	value, err = op.Execute([]string{"l", "8e2", "a", "0"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != -42 {
 		t.Errorf("Execute failed. Got %v, expected -42.", value)
@@ -25,12 +25,12 @@ func TestOperate(t *testing.T) {
 func TestOperate2(t *testing.T) {
 	op, err := NewOperate("a^b")
 	if err != nil {
-		t.Fatalf("NewOperate failed. err:%v", err)
+		t.Fatalf("NewOperate failed. err:%v.", err)
 	}
 
 	value, err := op.Execute([]string{"a", "10", "b", "1"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != 10 {
 		t.Errorf("Execute failed. Got %v, expected 10.", value)
@@ -38,7 +38,7 @@ func TestOperate2(t *testing.T) {
 
 	value, err = op.Execute([]string{"a", "10", "b", "2"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != 100 {
 		t.Errorf("Execute failed. Got %v, expected 100.", value)
@@ -46,7 +46,7 @@ func TestOperate2(t *testing.T) {
 
 	value, err = op.Execute([]string{"a", "100", "b", "2"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != 10000 {
 		t.Errorf("Execute failed. Got %v, expected 10000.", value)
@@ -56,12 +56,12 @@ func TestOperate2(t *testing.T) {
 func TestOperate3(t *testing.T) {
 	op, err := NewOperate("a√b")
 	if err != nil {
-		t.Fatalf("NewOperate failed. err:%v", err)
+		t.Fatalf("NewOperate failed. err:%v.", err)
 	}
 
 	value, err := op.Execute([]string{"a", "100", "b", "1"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != 100 {
 		t.Errorf("Execute failed. Got %v, expected 100.", value)
@@ -69,7 +69,7 @@ func TestOperate3(t *testing.T) {
 
 	value, err = op.Execute([]string{"a", "100", "b", "2"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != 10 {
 		t.Errorf("Execute failed. Got %v, expected 10.", value)
@@ -77,7 +77,7 @@ func TestOperate3(t *testing.T) {
 
 	value, err = op.Execute([]string{"a", "10000", "b", "2"})
 	if err != nil {
-		t.Fatalf("Execute failed. err:%v", err)
+		t.Fatalf("Execute failed. err:%v.", err)
 	}
 	if value != 100 {
 		t.Errorf("Execute failed. Got %v, expected 100.", value)
@@ -87,18 +87,18 @@ func Benchmark_Execute(b *testing.B) {
 	op, err := NewOperate("(-2.2-(l/2E1))I")
 	for d := 0; d < b.N; d++ {
 		if err != nil {
-			b.Fatalf("NewOperate failed. err:%v", err)
+			b.Fatalf("NewOperate failed. err:%v.", err)
 		}
 		value, err := op.Execute([]string{"l", "60"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != -5 {
 			b.Errorf("Execute failed. Got %v, expected -5.", value)
 		}
 		value, err = op.Execute([]string{"l", "8e2"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != -42 {
 			b.Errorf("Execute failed. Got %v, expected -42.", value)
@@ -110,12 +110,12 @@ func Benchmark_Execute2(b *testing.B) {
 	op, err := NewOperate("a^b")
 	for d := 0; d < b.N; d++ {
 		if err != nil {
-			b.Fatalf("NewOperate failed. err:%v", err)
+			b.Fatalf("NewOperate failed. err:%v.", err)
 		}
 
 		value, err := op.Execute([]string{"a", "10", "b", "1"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != 10 {
 			b.Errorf("Execute failed. Got %v, expected 10.", value)
@@ -123,7 +123,7 @@ func Benchmark_Execute2(b *testing.B) {
 
 		value, err = op.Execute([]string{"a", "10", "b", "2"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != 100 {
 			b.Errorf("Execute failed. Got %v, expected 100.", value)
@@ -131,7 +131,7 @@ func Benchmark_Execute2(b *testing.B) {
 
 		value, err = op.Execute([]string{"a", "100", "b", "2"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != 10000 {
 			b.Errorf("Execute failed. Got %v, expected 10000.", value)
@@ -142,12 +142,12 @@ func Benchmark_Execute3(b *testing.B) {
 	op, err := NewOperate("a√b")
 	for d := 0; d < b.N; d++ {
 		if err != nil {
-			b.Fatalf("NewOperate failed. err:%v", err)
+			b.Fatalf("NewOperate failed. err:%v.", err)
 		}
 
 		value, err := op.Execute([]string{"a", "100", "b", "1"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != 100 {
 			b.Errorf("Execute failed. Got %v, expected 100.", value)
@@ -155,7 +155,7 @@ func Benchmark_Execute3(b *testing.B) {
 
 		value, err = op.Execute([]string{"a", "100", "b", "2"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != 10 {
 			b.Errorf("Execute failed. Got %v, expected 10.", value)
@@ -163,7 +163,7 @@ func Benchmark_Execute3(b *testing.B) {
 
 		value, err = op.Execute([]string{"a", "10000", "b", "2"})
 		if err != nil {
-			b.Fatalf("Execute failed. err:%v", err)
+			b.Fatalf("Execute failed. err:%v.", err)
 		}
 		if value != 100 {
 			b.Errorf("Execute failed. Got %v, expected 100.", value)
